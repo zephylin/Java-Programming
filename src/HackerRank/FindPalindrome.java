22889ce6ed1e7b4df80c1bb38ba0aaacd4cd1c6d
zephylin <dzephylin@gmail.com>
2025-02-08 23:57:39 -0600

    added files

---
+++ src/HackerRank/FindPalindrome.java
@@ -0,0 +1,19 @@
+package HackerRank;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+public class FindPalindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {123,144,515,111,6};
+		System.out.println(findPalindromes(arr));
+
+	}
+	
+	public static int[] findPalindromes(int[] arr) {
+		List<Integer> res = new ArrayList<>();
+		
+	}
+
+}
