b080c56bb94f8dfcb3560346d58fab3ed6f6f922
zephylin <dzephylin@gmail.com>
2025-02-10 23:56:55 -0600

    added files

---
+++ src/Practice_Qtns/ReverseLinkedList.java
@@ -0,0 +1,29 @@
+package Practice_Qtns;
+
+public class ReverseLinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	class ListNode {
+	    int val;
+	    ListNode next;
+	    ListNode(int val) { this.val = val; }
+	}
+
+	public class ReverseLinkedList {
+	    public static ListNode reverseList(ListNode head) {
+	        ListNode prev = null, current = head;
+	        while (current != null) {
+	            ListNode nextTemp = current.next;
+	            current.next = prev;
+	            prev = current;
+	            current = nextTemp;
+	        }
+	        return prev;
+	    }
+	}
+
+
+}
